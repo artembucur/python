ca2a327b94778551be8b332ba47e17167a7f8847
lyubomyr83 <lyubomyr83@mail.ru>
2019-10-01 18:01:31 +0300

    Rodjer

--- rodjer.py
+++ rodjer.py
@@ -1,3 +1,5 @@
+from random import randint, choice
+
 print('Привет, меня зовут Роджер. А как тебя?')
 name = input() # имя
 name = name.title()
@@ -17,22 +19,52 @@ if ready == 'да':
     examples_quantity = '' # количество примеров
     max_answer = '' # до скольки будем считать
 
-    while examples_quantity.isdigit() != True:
+    while not examples_quantity.isdigit():
         print(name + ', сколько примеров ты готов решить?')
         examples_quantity = input()
-        if examples_quantity.isdigit() != True:
-            print('Ты ошибся, должна быть цифра.')
+        if examples_quantity.isdigit():
+            while int(examples_quantity) < 1:
+                print('Введи число больше 0 ')
+                examples_quantity = input()
+                while not examples_quantity.isdigit():
+                    print('Ты ошибся, должна быть цифра.')
+                    examples_quantity = input()
 
-    while max_answer.isdigit() != True:
+    while not max_answer.isdigit():
         print('До скольки будем считать? Например, до 100')
         max_answer = input()
-        if max_answer.isdigit() != True:
-            print('Ты ошибся, должна быть цифра.')
+        if max_answer.isdigit():
+            while int(max_answer) < 2:
+                print('Введи число больше 1')
+                max_answer = input()
+                while not max_answer.isdigit():
+                    print('Ты ошибся, должна быть цифра.')
+                    max_answer = input()
 
     print('Хорошо, тогда начинаем...')
 
     for example in range(int(examples_quantity)):
+
+        number1 = randint(1, int(max_answer))
+        number2 = randint(1, int(max_answer))
+        sign = choice('+-')
+
         print('пример ' + str(example+1))
+
+        if sign == '-':
+            while number1 < number2:
+                number1 = randint(1, int(max_answer))
+
+        if sign == '+':
+            while number1 + number2 > 100:
+                number1 = randint(1, int(max_answer))
+                number2 = randint(1, int(max_answer))
+
+
+        print(str(number1)+sign+str(number2))
+
+
+
 
 elif ready == 'нет':
     print('Передумал? Хорошо, может как-нибудь в следующий раз...')
